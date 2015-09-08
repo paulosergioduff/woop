ac4992580c1edaa4558cb89697cf80178ddbb6c8
paulosergioduff <paulosergioduff@gmail.com>
2015-09-08 03:35:29 -0300

    Update exemplo2.php

--- exemplo2.php
+++ exemplo2.php
@@ -3,15 +3,16 @@
 include "criarpagina.class.php";
 
 
-$app = 'EscreveNoFinal';
+$app = 'Update';
 
 	$criarpagina = new criarpagina; /* A função de usar classes e objetos ao invés de variáveis estruturais
 	tem mais a ver com didática e organização do código*/
 
-	$criarpagina->pagina = 'arquivo_de_classe.txt';
+	$criarpagina->pagina = 'meuarquivo.txt';
 	$criarpagina->conteudo = 'Conteudo novo criado!';
 	//$criarpagina->arquivoalvo = ';
 	$criarpagina->$app();
+	echo "<p>Resultado:<br>"; include "meuarquivo.txt";
 	//$criarpagina->imprimeXML();
 
 ?> 
